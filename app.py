ab2d250b9ec8fa602e75fedc21532be60910f185
faeze azimi <68439966+FaezeAzimi@users.noreply.github.com>
2023-01-15 09:56:44 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import numpy as np
 import random
+import pkgaware as pkga
 import pkgmon as mon
 import numpy as np
-
-from pkgaware.Discrete import sample as pkgasample
-from pkgaware.FuncEnv import step as pkgastep
-
 
 def random_function_gen(steps):
     for x in range(0,steps):
@@ -19,7 +16,7 @@ def update_state(state, state2, reward, action, action2):
 def choose_action(state,action,abso):
         action=0
         if np.random.uniform(0, 1) < epsilon:
-                action = pkgasample(state,action,reward)
+                action = pkga.sample(state,action,reward)
         else:
                 action = np.argmax(Q[state, :])
         return action
@@ -28,11 +25,11 @@ def SFSchlr(queue):
         abso=1
         for func in queue:
                 t = 0
-#                state1 = pkga.FuncEnv()
+                state1 = pkga.FuncEnv()
                 st1=state1.reset()
                 action1 = choose_action(st1,ch_action,abso)
                 while t < max_steps:
-                        state2, reward, abso, done = pkgastep(action1,st1,func,func_path)
+                        state2, reward, abso, done = pkga.step(action1,st1,func,func_path)
                         action2 = choose_action(state2)
                         st1 = state2
                         action1 = action2
@@ -47,7 +44,7 @@ alpha = 0.6
 gamma = 0.7
 initial_container=5
 inittal_packages=0
-queue1=['f4','f1','f2','f3','f5','f6']
+queue1=['f4','f1','f2','f3','f2']
 reward = 0
 ch_action='A1'
 
